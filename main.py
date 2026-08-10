62dbd4b662e7fd82c20c1c6dced4a746e89dc83c
Sophia Mattos <ssigolini@gmail.com>
2026-08-10 17:26:14 -0300

    Add Pokemon search and error handling

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import requests 
 
 print("PokéTeam iniciado!")
-url = "https://pokeapi.co/api/v2/pokemon/pikachu"
+pokemon = input("Choose a Pokémon: ")
+url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
 
 resposta = requests.get(url)
 
-dados = resposta.json()
+if resposta.status_code == 200: 
+    print("We have your pokémon here!")
+    dados = resposta.json()
+    
+    nome = dados["name"]
+    altura = int(dados["height"])
+    peso = int(dados["weight"])
 
-nome = dados["name"]
-altura = int(dados["height"])
-peso = int(dados["weight"])
+    print(f"Nome: {nome}")
+    print(f"Altura: {altura / 10} m")
+    print(f"Peso: {peso / 10} kg")
+    print(f"Seu pokémon é do tipo: {dados["types"][0]["type"]["name"]}")
 
-print(f"Nome: {nome}")
-print(f"Altura: {altura / 10} m")
-print(f"Peso: {peso / 10} kg")
-
-print(f"Seu pokémon é do tipo: {dados["types"][0]["type"]["name"]}")
+else:
+    print("Yeah, we don't have it...")
